15250c8f294132c4f4c2f614bf063f205844fcd7
littleFootDev <thelittlefootdev@gmail.com>
2022-06-07 15:09:39 +0200

    adding database with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,6 +37,8 @@ import { RecipeService } from './recipes/recipe.service';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule
+    
   ],
   providers: [SoppingListService, RecipeService],
   bootstrap: [AppComponent]
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import { IRecipe } from "../recipes/recipe.interface";
+import { RecipeService } from "../recipes/recipe.service";
+import { map, tap} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataStorageService {
+    constructor(
+        private http: HttpClient,
+        private recipeService: RecipeService
+    ){};
+
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+
+        this.http.post('https://littlefootbookrecipes-88ef8-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipes).subscribe(response => {
+        
+        });
+    }
+
+    fetchRecipes() {
+        return this.http
+            .get<IRecipe[]>('https://littlefootbookrecipes-88ef8-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+            .pipe(
+                map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients : recipe.ingredients ? recipe.ingredients : []};
+                });
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                } )
+            );
+            
+    }
+}
